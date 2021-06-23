dbcff8c66cad43ac56d858962eaee95ea27eb3d6
João Victor Mendes <mendesjoaovic@gmail.com>
2021-06-23 09:59:58 -0300

    Fixed last commit to show both

--- Challenge 1/convert_hex_to_b64.py
+++ Challenge 1/convert_hex_to_b64.py
@@ -2,9 +2,10 @@ import codecs
 
 def hex_to_b64(encoded_hex):
     decoded = codecs.decode(encoded_hex, 'hex')
+    print(decoded)
     b64 = codecs.encode(decoded, 'base64')
-    return b64
+    return b64.decode()
 
 encoded_hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 transformed = hex_to_b64(encoded_hex)
-print(codecs.decode(transformed, 'base64'))
+print(transformed)
